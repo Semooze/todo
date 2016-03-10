493fbd2e9540c1e33df3d9926f12a5ccb9055782
semooze <semoozelittle@yahoo.com>
2016-03-10 22:44:52 +0700

    Add status property into ToDo's model and add function change status to doing or done

--- js/app.js
+++ js/app.js
@@ -1,21 +1,55 @@
+var filters = {
+  do: function(todos) {
+    return todo.status === 'do';
+  },
+  doing: function(todos) {
+    return todos.filter(function(todo) {
+      return todo.status === 'doing';
+    });
+  },
+  done: function(todos) {
+    return todos.filter(function(todo) {
+      return todo.status === 'done';
+    });
+  },
+};
+
+
 new Vue({
   el: '#app',
   data: {
     newTodo: '',
     todos: [
-      {text: 'Add some todos'}
+      {text: 'Add some todos', status: 'do'}
     ]
   },
+  computed: {
+   all: function() {
+     return this.todos;
+   },
+   remaining: function () {
+		return filters.do(this.todos).length;
+	 },
+   doingFilter: function() {
+    return
+   }
+  },
   methods: {
-    addTodo: function() {
+    addTask: function() {
       var text = this.newTodo.trim();
       if (text) {
-        this.todos.push({text: text});
+        this.todos.push({text: text, status: 'do'});
         this.newTodo = '';
       }
     },
-    removeTodo: function(index) {
+    removeTask: function(index) {
       this.todos.splice(index, 1);
+    },
+    doingTask: function(index) {
+      this.todos[index].status = 'doing';
+    },
+    completeTask: function(index) {
+      this.todos[index].status = 'done';
     }
   }
 });
