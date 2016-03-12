7474d65d8239d75910cde8bbc6665bad65ddba90
semooze <semoozelittle@yahoo.com>
2016-03-12 10:08:10 +0700

    Add error exception when minify javascript

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
+'use strict';
+
 var gulp = require('gulp'),
   uglify = require('gulp-uglify'),
-  rename = require('gulp-rename');
+  sass = require('gulp-sass'),
+  rename = require('gulp-rename'),
   livereload = require('gulp-livereload');
 
 gulp.task('default', function() {
@@ -13,14 +16,24 @@ gulp.task('reload', function() {
 
 gulp.task('compress', function() {
   return gulp.src('js/*.js')
-    .pipe(uglify())
+    .pipe(uglify().on('error', function(e) {
+      console.log('Error message:', e.message, 'Line number:', e.lineNumber);
+    }))
     .pipe(rename({
       suffix: '.min'
     }))
     .pipe(gulp.dest('dist'));
 });
 
-var watcher = gulp.watch('js/*.js', ['compress', 'reload']);
+var watcher = gulp.watch('./js/*.js', ['compress', 'reload']);
 watcher.on('change', function(event) {
   console.log('File ' + event.path + ' was ' + event.type);
 });
+
+gulp.task('sass', function () {
+  return gulp.src('./sass*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.watch('./sass/*.scss', ['sass']);
