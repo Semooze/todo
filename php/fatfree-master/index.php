239c4358fcdac7be00233192f5539b5009886373
semooze <semoozelittle@yahoo.com>
2016-03-13 12:21:34 +0700

    Copy todo application to use in php and delete unneccessary files

--- php/fatfree-master/index.php
+++ php/fatfree-master/index.php
@@ -10,6 +10,14 @@ if ((float)PCRE_VERSION<7.9)
 // Load configuration
 $f3->config('config.ini');
 
+//config database connection
+$db=new DB\SQL(
+    'mysql:host=localhost;port=3306;dbname=todo',
+    'root',
+    '123456'
+);
+$f3->set('db', $db);
+
 $f3->route('GET /',
 	function($f3) {
 		$classes=array(
@@ -77,6 +85,33 @@ $f3->route('GET /userref',
 		$f3->set('content','userref.htm');
 		echo View::instance()->render('layout.htm');
 	}
+);
+
+$f3->route('GET /',
+    function() {
+        echo View::instance()->render('index.htm');
+    }
+);
+
+$f3->route('POST /tasks/save',
+    function() {
+      echo $_SERVER['SERVER_NAME']. ':' . $_SERVER['SERVER_PORT'];
+    }
+);
+$f3->route('GET /tasks/load',
+    function() {
+      echo $_SERVER['SERVER_NAME']. ':' . $_SERVER['SERVER_PORT'];
+    }
+);
+
+$f3->route('GET /test',
+    function($f3) {
+      $db = $f3->get('db');
+      $results = $db->exec('SELECT * FROM tasks');
+      $resultsJson = json_encode($results);
+      $resultsString = json_encode(array('todos' => $resultsJson), JSON_FORCE_OBJECT);
+      var_dump($resultsString);
+    }
 );
 
 $f3->run();
---
+++ php/fatfree-master/ui/js/app.js
@@ -0,0 +1,134 @@
+Vue.directive('todo-focus', function (value) {
+  if (!value) {
+    return;
+  }
+  var el = this.el;
+  Vue.nextTick(function () {
+    el.focus();
+  });
+});
+
+
+var filters = {
+  all: function (todos) {
+    return todos;
+  },
+  active: function (todos) {
+    return todos.filter(function (todo) {
+      return todo.status === 'active';
+    });
+  },
+  completed: function (todos) {
+    return todos.filter(function (todo) {
+      return todo.status === 'completed';
+    });
+  }
+};
+
+new Vue({
+  el: '.todoapp',
+  data: {
+    newTodo: '',
+    todos: [],
+    editedTodo: null,
+    visibility: todoStorage.fetch()
+  },
+  watch: {
+    visibility: {
+      handler: todoStorage.save
+    }
+  },
+  computed: {
+    filteredTasks: function () {
+      return filters[this.visibility](this.todos);
+    },
+    remaining: function () {
+      return filters.active(this.todos).length;
+    },
+    date: function () {
+      var dateObj = new Date();
+      var date = dateObj.getDate();
+      var month = dateObj.getMonth() + 1;
+      var year = dateObj.getFullYear();
+      var result = date + '/' + month + '/' + year;
+      return result;
+    }
+  },
+  methods: {
+    addTask: function() {
+      var text = this.newTodo.trim();
+      if (text) {
+        this.todos.push({text: text, status: 'active', createTime: new Date().getTime()});
+        this.newTodo = '';
+      }
+    },
+    removeTask: function(todo) {
+      var todos = this.todos;
+      todos.forEach(function (t, index) {
+        if (todo === t) {
+          return todos.splice(index, 1);
+        }
+      });
+    },
+    editTask: function (todo) {
+      this.beforeEditCache = todo.text;
+      this.editedTodo = todo;
+    },
+    doneEdit: function (todo) {
+      if (!this.editedTodo) {
+        return;
+      }
+      this.editedTodo = null;
+      todo.text = todo.text.trim();
+      if (!todo.text) {
+        this.removeTodo(todo);
+      }
+    },
+    cancelEdit: function (todo) {
+      this.editedTodo = null;
+      todo.text = this.beforeEditCache;
+    },
+    completeTask: function(todo) {
+      todo.status = 'completed';
+    },
+    removeAllTasks: function () {
+      this.todos = [];
+    },
+    setVisibility: function (value) {
+      this.visibility = value;
+    },
+    importTasks: function(e) {
+      var todos = this.todos;
+      var files = e.target.files;
+      for (var i = 0, file; file = files[i]; i++) {
+
+        if (!file.type.match('application/json')) {
+          continue;
+        }
+        var reader = new FileReader();
+        reader.onload = function(e) {
+          var data = JSON.parse(e.target.result);
+          if (!data.todos) return console.error('content format is not match');
+          data.todos.forEach(function (todo) {
+            todos.push(todo);
+          });
+        };
+        reader.onerror = (function(error) {
+          if (error) return console.log(error);
+        })();
+        reader.readAsText(file, 'UTF-8');
+      }
+    },
+    exportTasks: function () {
+      var data = {};
+      data.todos = this.todos;
+      var a         = document.createElement('a');
+      a.href        = 'data:application/json;charset=utf-8,' +  encodeURIComponent(JSON.stringify(data));
+      a.target      = '_blank';
+      a.download    = 'todos.json';
+
+      document.body.appendChild(a);
+      a.click();
+    }
+  }
+});
